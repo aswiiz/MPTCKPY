3ce178a2c0189b3fd8d0b1771b992d15552b3f51
MPTC Dev <aswi@mptc.edu>
2026-06-22 14:08:49 +0000

    Auto-create admin superuser on deploy

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Auto-create superuser if it doesn't exist
+echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@mptc.com', 'admin@123')" | python manage.py shell
