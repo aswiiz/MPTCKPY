dc4cc1d5ee5d2b25d1a86917f38b787883c118e1
ASWIN RAJ <greenmovies325@gmail.com>
2026-06-29 13:15:51 +0530

    Update settings.py

--- mptc/settings.py
+++ mptc/settings.py
@@ -29,8 +29,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-^mfo47tksc(j(_gmjz&b-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
-
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "mptckpy.vercel.app",
+]
 
 # Application definition
 
